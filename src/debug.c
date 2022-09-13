552b455997b9f3b6dbf8bb743869b0c64cba5d82
priyaan99 <priyaanyadav@gmail.com>
2022-09-13 16:25:53 +0530

    impl 14.5 constant

---
+++ src/debug.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#include "debug.h"
+#include "value.h"
+
+void disassemble_chunk(Chunk* chunk, const char* name) {
+	printf("== %s ==\n", name);
+
+	for (int offset = 0; offset < chunk->count; ) {
+		offset = disassemble_instruction(chunk, offset);
+	}
+}
+
+static int constant_instruction(const char* name, Chunk* chunk, int offset) {
+	uint8_t constant = chunk->code[offset + 1]; 
+	printf("%-16s %4d '", name, constant);
+	print_value(chunk->constants.values[constant]);
+	printf("'\n");
+
+	return offset + 2;
+}
+
+static int simple_instruction(const char* name, int offset) {
+	printf("%s\n", name);
+	return offset + 1;
+}
+
+int disassemble_instruction(Chunk* chunk, int offset) {
+	printf("%04d ", offset);
+
+	uint8_t instruction = chunk->code[offset];
+	switch (instruction) {
+		case OP_CONSTANT:
+			return constant_instruction("OP_RETURN", chunk, offset);
+		case OP_RETURN:
+			return simple_instruction("OP_RETURN", offset);
+		default:
+			printf("Unknown Opcode %d\n", instruction);
+			return offset + 1;
+	}
+}
--- src/main.c
+++ src/main.c
@@ -1,5 +1,18 @@
 #include "common.h"
+#include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
+	Chunk chunk;
+	init_chunk(&chunk);
+	write_chunk(&chunk, OP_RETURN);
+
+	int constant = add_constant(&chunk, 1.2);
+	write_chunk(&chunk, OP_CONSTANT);
+	write_chunk(&chunk, constant);
+	
+	disassemble_chunk(&chunk, "test chunk");
+
+	free_chunk(&chunk);
     return 0;
 }
