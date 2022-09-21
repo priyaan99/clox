26bcf78dbee8002916285aa77e5b07d85394a79d
priyaan99 <priyaanyadav@gmail.com>
2022-09-21 15:16:25 +0530

    impl numbers and string token scanner 16.3 done

--- src/scanner.c
+++ src/scanner.c
@@ -19,6 +19,10 @@ void init_scanner(const char* source) {
 	return;
 }
 
+static bool is_digit(char c) {
+	return c >= '0' && c <= '9';
+}
+
 static bool is_at_end() {
 	return *scanner.current == '\0';
 }
@@ -26,6 +30,22 @@ static bool is_at_end() {
 static char advance() {
 	scanner.current++;
 	return scanner.current[-1];
+}
+
+static char peek() {
+	return *scanner.current;
+}
+
+static char peek_next() {
+	if (is_at_end()) return '\0';
+	return scanner.current[1];
+}
+
+static bool match(char expected) {
+	if (is_at_end()) return false;
+	if (*scanner.current != expected) return false;
+	scanner.current++;
+	return true;
 }
 
 static Token make_token(TokenType type) {
@@ -46,12 +66,70 @@ static Token error_token(const char* message) {
 	return token;
 }
 
+static void skip_whitespace() {
+	for (;;) {
+		char c = peek();
+		switch (c) {
+			case ' ':
+			case '\r':
+			case '\t':
+				advance();
+				break;
+			case '\n':
+				scanner.line++;
+				advance();
+				break;
+
+			case '/':
+				if (peek_next() == '/') {
+					while (peek() != '\n' && !is_at_end()) advance();
+				} else {
+					return;
+				}
+				break;
+			default:
+				return;
+		}
+	}
+}
+
+static Token number() {
+	while (is_digit(peek())) advance();
+
+	// look for a factional part.
+	if (peek() == '.' && is_digit(peek_next())) {
+		// consume the "."
+		advance();
+
+		while (is_digit(peek())) advance();
+	}
+
+	return make_token(TOKEN_NUMBER);
+	
+}
+
+static Token string() {
+	while (peek() != '"' && !is_at_end()) {
+		if (peek() == '\n') scanner.line++;
+		advance();
+	}
+
+	if (is_at_end()) return error_token("Unterminated string.");
+
+	// for closing quote.
+	advance();
+	return make_token(TOKEN_STRING);
+}
+
 Token scan_token() {
+	skip_whitespace();
 	scanner.start = scanner.current;
 
 	if (is_at_end()) return make_token(TOKEN_EOF);
 
 	char c = advance();
+
+	if (is_digit(c)) return number();
 
 	switch (c) {
 		case '(': return make_token(TOKEN_LEFT_PAREN);
@@ -65,6 +143,21 @@ Token scan_token() {
 		case '+': return make_token(TOKEN_PLUS);
 		case '/': return make_token(TOKEN_SLASH);
 		case '*': return make_token(TOKEN_STAR);
+		case '!':
+		  return make_token(
+			  match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+		case '=':
+		  return make_token(
+			  match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+		case '<':
+		  return make_token(
+			  match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+		case '>':
+		  return make_token(
+			  match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+
+		case '"': return string();
+
 	}
 
 	return error_token("Unexpected character.");
