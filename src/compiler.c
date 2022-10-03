f1f12c7488c1c15174067c724e93d86a070934f6
priyaan99 <priyaanyadav@gmail.com>
2022-10-03 15:30:01 +0530

    impl write bytes to chunk : emit_byte() 17.3

--- src/compiler.c
+++ src/compiler.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -12,6 +13,12 @@ typedef struct {
 } Parser;
 
 Parser parser;
+
+Chunk* compiling_chunk;
+
+static Chunk* current_chunk() {
+	return compiling_chunk;
+}
 
 static void error_at(Token* token, const char* message) {
 	if (parser.panic_mode) return;
@@ -29,6 +36,10 @@ static void error_at(Token* token, const char* message) {
 
 	fprintf(stderr, ": %s\n", message);
 	parser.had_error = true;
+}
+
+static void error(const char* message) {
+	error_at(&parser.previous, message);
 }
 
 static void error_at_current(const char* message) {
@@ -56,8 +67,50 @@ static void consume(TokenType type, const char* message) {
 	error_at_current(message);
 }
 
+static void emit_byte(uint8_t byte) {
+	write_chunk(current_chunk(), byte, parser.previous.line);
+}
+
+static void emit_bytes(uint8_t byte1, uint8_t byte2) {
+	emit_byte(byte1);
+	emit_byte(byte2);
+}
+
+static void emit_return() {
+	emit_byte(OP_RETURN);
+}
+
+static uint8_t make_constant(Value value) {
+	int constant = add_constant(current_chunk(), value);
+	if (constant > UINT8_MAX) {
+		error("Too many constants in one chunk");
+		return 0;
+	}
+
+	return (uint8_t) constant;
+}
+
+static void emit_constant(Value value) {
+	emit_bytes(OP_CONSTANT, make_constant(value));
+}
+
+static void end_compiler() {
+	emit_return();
+}
+
+/// Expression Parsing ///
+
+static void number() {
+	double value = strtod(parser.previous.start, NULL);
+	emit_constant(value);
+}
+
+static void expression() {
+}
+
 bool compile(const char* source, Chunk* chunk) {
 	init_scanner(source);
+	compiling_chunk = chunk;
 
 	parser.had_error = false;
 	parser.panic_mode = false;
@@ -65,6 +118,8 @@ bool compile(const char* source, Chunk* chunk) {
 	advance();
 	expression();
 	consume(TOKEN_EOF, "Expect end of Expression");
+
+	end_compiler();
 
 	return !parser.had_error;
 }
