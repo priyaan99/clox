90444951454c9f32ef85797b2f4dd1cd0e5b92ce
priyaan99 <priyaanyadav@gmail.com>
2022-11-06 10:00:33 +0530

    impl string obj tyep 19.3

---
+++ src/object.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "memory.h"
+#include "object.h"
+#include "value.h"
+#include "vm.h"
+
+#define ALLOCATE_OBJ(type, object_type) \
+	(type*) allocate_obj(sizeof(type), object_type)
+
+static Obj* allocate_obj(size_t size, ObjType type) {
+	Obj* object = (Obj*)reallocate(NULL, 0, size);
+	object->type = type;
+	return object;
+}
+
+static ObjString* allocate_string(char* chars, int length) {
+	ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
+	string->length = length;
+	string->chars = chars;
+	return string;
+}
+
+ObjString* copy_string(const char* chars, int length) {
+	char* heap_chars = ALLOCATE(char, length+1);
+	memcpy(heap_chars, chars, length);
+	heap_chars[length] = '\0';
+	return allocate_string(heap_chars, length);
+}
